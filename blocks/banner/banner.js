2ae447db064646a3f823ccb4b395bfb29c82806c
Lamont Crook <lamont@adobe.com>
2024-09-24 16:08:52 -0600

    add banner

---
+++ blocks/banner/banner.js
@@ -0,0 +1,19 @@
+export default function decorate(block) {
+  const buttonNameDiv = block.querySelector('div:nth-child(3)');
+  const buttonName = buttonNameDiv.textContent.trim();
+  buttonNameDiv.remove();
+  block.querySelector('.button-container').textContent = buttonName;
+
+  const div = document.createElement('div');
+  div.classList.add('promo-conditions');
+
+  const pc = document.createElement('a');
+  pc.href = 'https://www.michaelkors.com/info/promo-conditions.html';
+
+  const p = document.createElement('p');
+  p.textContent = 'PRICES AS MARKED | PRODUCT EXCLUSIONS APPLY | TERMS';
+
+  pc.append(p);
+  div.append(pc);
+  block.append(div);
+}
