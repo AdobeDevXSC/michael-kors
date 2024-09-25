c179a0edbf8eab972985c9a59e851402482d7aa3
Lamont Crook <lamont@adobe.com>
2024-09-25 12:51:51 -0600

    update lint

---
+++ blocks/hero/hero.js
@@ -0,0 +1,11 @@
+/**
+ * loads and decorates the header, mainly the nav
+ * @param {Element} block The header block element
+ */
+export default async function decorate(block) {
+  const sections = [...block.children].length === 3 ? ['divider', 'image', 'text'] : ['image', 'text'];
+  [...block.children].forEach((row) => {
+    const s = sections.shift();
+    row.classList.add(s);
+  });
+}
